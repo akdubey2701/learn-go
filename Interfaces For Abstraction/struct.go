3cb9a1125c9b4818fd714a404bfc41165fdd3c5a
akdubey2701 <akdubey2701@gmail.com>
2020-05-25 13:34:13 +0530

    Update struct.go

--- Interfaces For Abstraction/struct.go
+++ Interfaces For Abstraction/struct.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	"fmt"
@@ -52,7 +52,7 @@ func main() {
 	/*	cow := Animal{food: "grass", locomotion: "walk", noise: "moo"}
 		bird := Animal{food: "worms", locomotion: "fly", noise: "peep"}
 		snake := Animal{food: "mice", locomotion: "slither", noise: "hsss"}
-	*/
+
 	for {
 
 		fmt.Printf("> ")
@@ -89,6 +89,67 @@ func main() {
 					}
 				}
 			}
+		}
+	}
+}
+*/
+package main
+
+import (
+	"fmt"
+)
+
+type animal struct {
+	food       string
+	locomotion string
+	noise      string
+}
+
+type animalInterface interface {
+	Eat()
+	Move()
+	Speak()
+}
+
+func (ani animal) Eat() {
+	fmt.Println(ani.food)
+	return
+}
+
+func (ani animal) Move() {
+	fmt.Println(ani.locomotion)
+	return
+}
+
+func (ani animal) Speak() {
+	fmt.Println(ani.noise)
+	return
+}
+
+func main() {
+	animalMap := make(map[string]animal)
+	animalMap["cow"] = animal{"grass", "walk", "moo"}
+	animalMap["bird"] = animal{"worms", "fly", "peep"}
+	animalMap["snake"] = animal{"mice", "slither", "hsss"}
+	var genralAni animalInterface
+	for {
+		var command, requestAni, requestType string
+		fmt.Print(">")
+		fmt.Scan(&command, &requestAni, &requestType)
+		if command == "query" {
+			genralAni = animalMap[requestAni]
+			switch requestType {
+			case "eat":
+				genralAni.Eat()
+			case "move":
+				genralAni.Move()
+			case "speak":
+				genralAni.Speak()
+			}
+		}
+		if command == "newanimal" {
+			animalMap[requestAni] = animalMap[requestType]
+			fmt.Println("Created it!")
 		}
 	}
 }
