d1cf484c1b58e8b560563c71aeed94e6449d4fe4
akdubey2701 <akdubey2701@gmail.com>
2020-05-28 14:28:51 +0530

    Update assignment2.go

--- Concurrency/assignment2.go
+++ Concurrency/assignment2.go
@@ -1,19 +1,25 @@
+//go run -race assignment2.go
+/*A race condition is an undesirable situation that occurs when a device or system attempts to perform two or more operations at the same time, but because of the nature of the device or system, the operations must be done in the proper sequence to be done correctly.*/
 package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func p(a, b, c int) {
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
 }
+
+// function f is called directly , function f and another anynomous function  are interleaved to show race condition
 func main() {
-	go p(1, 2, 3)
-	go p(4, 5, 6)
-
+	f("call executed directly")
+	go f("goroutine")
+	go func(msg string) {
+		fmt.Println(msg)
+	}("go routine interleaved")
+	time.Sleep(time.Second)
+	fmt.Println("done")
 }
-
-//go run -race assignment2.go
-/*A race condition is an undesirable situation that occurs when a device or system attempts to perform two or more operations at the same time, but because of the nature of the device or system, the operations must be done in the proper sequence to be done correctly.*/
