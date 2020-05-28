30e3335b6b56553aeb253ec403f7572ec8679e39
akdubey2701 <akdubey2701@gmail.com>
2020-05-28 14:28:47 +0530

    Update assignment_3.go

--- Concurrency/assignment_3.go
+++ Concurrency/assignment_3.go
@@ -2,52 +2,102 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"math"
+	"strconv"
+	"strings"
+	"sync"
 )
 
-func main() {
-	size := 20
-	slice := generateSlice(size)
-	fmt.Println("\n--- Unsorted --- \n\n", slice)
-	fmt.Println("\n--- Sorted ---\n\n", Sort_func(slice, size), "\n")
-}
+func inputList() []int {
+	list := make([]int, 0, 4)
 
-func generateSlice(size int) []int {
+	for {
+		fmt.Print("Enter a number, or 'X' or 'x' to exit: ")
+		var s string
+		fmt.Scanf("%s", &s)
+		s = strings.ToLower(s)
 
-	slice := make([]int, size, size)
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < size; i++ {
-		slice[i] = rand.Intn(999) - rand.Intn(999)
+		if s == "x" {
+			break
+		}
+
+		v, err := strconv.Atoi(s)
+
+		if err != nil {
+			fmt.Printf("%s, try again\n", err)
+			continue
+		}
+
+		list = append(list, int(v))
+
 	}
-	return slice
+
+	return list
 }
-func Sort_func(slice []int, size int) []int {
 
-	l0 := 0
-	l1 := int(size / 4)
-	l2 := int(size/4 + 1)
-	l3 := int(size / 2)
-	l4 := int(size/2 + 1)
-	l5 := int(3 * size / 4)
-	l6 := int(3*size/4 + 1)
-	l7 := size - 1
-	go sort(slice, l0, l1)
-	go sort(slice, l2, l3)
-	go sort(slice, l4, l5)
-	go sort(slice, l6, l7)
+// This is from a previous assigment, so I thought I'd used it instead of sort.Ints
+func bubbleSort(list []int, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	sort(slice, 0, size-1)
+	fmt.Printf("sorting sublist %v\n", list)
 
-	return slice
-}
-func sort(sli []int, s, e int) []int {
-	for i := s; i <= e; i++ {
-		for j := s; j <= e-i-1; j++ {
-			if sli[j] > sli[j+1] {
-				sli[j], sli[j+1] = sli[j+1], sli[j]
+	for i := 0; i < len(list)-1; i++ {
+		for j := 0; j < len(list)-i-1; j++ {
+			if list[j] > list[j+1] {
+				swap(list, j)
 			}
 		}
 	}
-	return sli
+}
+
+func swap(sli []int, i int) {
+	temp := sli[i]
+	sli[i] = sli[i+1]
+	sli[i+1] = temp
+}
+
+func removeMin(sublists [][]int) int {
+	mini := -1
+	minv := int(math.MaxInt32)
+	for i, sublist := range sublists {
+		if len(sublist) > 0 && sublist[0] < minv {
+			mini = i
+			minv = sublist[0]
+		}
+	}
+
+	// remove the smallest value from its respective sublist
+	if mini != -1 {
+		sublists[mini] = sublists[mini][1:]
+	}
+
+	return minv
+}
+
+func main() {
+
+	list := inputList()
+	sublists := make([][]int, 4)
+
+	// chop up list into as evenly sized pieces as possible
+	sublists[0] = list[:len(list)/4]
+	sublists[1] = list[len(list)/4 : len(list)/2]
+	sublists[2] = list[len(list)/2 : 3*len(list)/4]
+	sublists[3] = list[3*len(list)/4:]
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+	go bubbleSort(sublists[0], &wg)
+	go bubbleSort(sublists[1], &wg)
+	go bubbleSort(sublists[2], &wg)
+	go bubbleSort(sublists[3], &wg)
+	wg.Wait()
+
+	sortedList := make([]int, 0, len(list))
+	for range list {
+		v := removeMin(sublists)
+		sortedList = append(sortedList, v)
+	}
+
+	fmt.Printf("sortedList: %v\n", sortedList)
 }
